0b3c8a47ed6ccc970f43ed2f9a6388f6e3a80503
katya <you@example.com>
2023-12-16 12:48:23 +0300

    edit

--- main.py
+++ main.py
@@ -10,17 +10,18 @@ chat_id = 5050226393
 
 @bot.message_handler(commands=['start'])
 def start_msg(message):
-    bot.reply_to(message, f"Привет {message.from_user.first_name}, я Тони Старк, жми /help чтобы начать со мной общение")
+    bot.reply_to(message, f"Привет {message.from_user.first_name}, я Тони Старк жми /help чтобы начать со мной общение")
     photo = open("C:/Users/lenovo/Downloads/tryToni.jpg", 'rb')
     bot.send_photo(chat_id, photo)
 
 @bot.message_handler(commands=['help'])
 def help_msg(message):
     bot.reply_to(message, "Могу рассказать вам о себе!\n"
-                          "Можете спросить кто я вообще такой? - /who - да кто ты такой без своего костюма?\n"
-                          "А могу вам мемы прислать по марвел - /memes - пришли ка мне мемчики\n"
-                          "А могу и рассказать о себе подробненько - /about - расскажи о себе\n"
-                          "Чтобы попрощаться, можешь написать 'пока', 'прощай' или 'до свидания'")
+                          "Можешь спросить кто я вообще такой? - /who - 'да кто ты такой без своего костюма'?\n"
+                          "А могу и мемы про марвел прислать - /memes - 'пришли ка мне мемчики'\n"
+                          "А могу и рассказать о своих хобби - /about - 'расскажи о себе!'\n"
+                          "Жми /details чтобы узнать обо мне чуточку больше))"
+                          "Чтобы попрощаться, можешь написать 'пока' или 'прощай'")
 
 @bot.message_handler(commands=['who'])
 def heh_msg(message):
@@ -29,25 +30,39 @@ def heh_msg(message):
     bot.send_photo(chat_id, photo)
 
 @bot.message_handler(commands=['memes'])
-def start_msg(message):
+def meme_msg(message):
     bot.reply_to(message, "Уан Секонд")
     photo = open("C:/Users/lenovo/Downloads/Tony_meme.jpg", 'rb')
     bot.send_photo(chat_id, photo)
+    bot.reply_to(message, "Жми /more чтобы я прислал еще один мемчик")
+
+@bot.message_handler(commands=['more'])
+def more_msg(message):
+    photo1 = open("C:/Users/lenovo/Downloads/meme2.jpg", 'rb')
+    bot.send_photo(chat_id, photo1)
 
 
 @bot.message_handler(commands = ['about'])
 def about_msg(message):
     for key, value in hobby_list.items():
-        bot.reply_to(message, f"{key} - {value['hobby']}")
+        bot.reply_to(message, f"{key}. {value['hobby']} - {value['description']}")
+
+@bot.message_handler(commands = ['details'])
+def about_msg(message):
+    for key, value in special_list.items():
+        bot.reply_to(message, f"{key} - {value}")
 
 @bot.message_handler(content_types = ['text'])
 def goodbye_msg(message):
     if message == 'пока' or 'прощай' or 'до свидания':
         bot.send_message(message.chat.id, "Всего хорошего, бывай")
 
-@bot.message_handler()
+@bot.message_handler(content_type = ['text'])
 def send_text(message):
-    bot.send_message(message.chat.id, 'Чтобы мы поговрили как надо, следуй инструкцийм которые я присылаю')
+    bot.send_message(message.chat.id, 'Чтобы мы поговрили как надо, следуй инструкциям которые я присылаю')
     bot.send_message(message.chat.id, 'введи что то что я умею распозновать, а то увы, наше общение здесь и закончится ')
+
+
+
 
 bot.polling()
